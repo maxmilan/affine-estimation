0b7f62bc59beabd57f761b3ed66f4ef51338ac5f
maxmilan <max231194@gmail.com>
2017-04-19 17:16:20 +0300

    Some updates

--- models/a_0_1.py
+++ models/a_0_1.py
@@ -5,7 +5,7 @@ class A01:
   def __init__(self, without_error_maturity=1.0/12, with_error_maturity=2.0):
     self._m = 0
     self._n = 1
-    self.b11, self.λ1, self.δ0, self.τ = var("b11 λ1 δ0 τ")
+    self.b_11, self.λ_1, self.δ_1, self.τ = var("b_11 λ_1 δ_1 τ")
     self._A = None
     self._B = None
     self.without_error_maturity = without_error_maturity
@@ -22,32 +22,32 @@ class A01:
   @property
   def A(self):
     if self._A is None:
-      expression = (self.δ0 * ((3 - 4 * exp(self.τ * self.b11) + exp(2 * self.τ * self.b11) + 2 * self.τ * self.b11) * self.δ0 - 4 * self.b11 * (1 - exp(self.τ * self.b11) + self.τ * self.b11) * self.λ1)) / (4 * self.b11**3)
+      expression = (self.δ_1 * ((3 - 4 * exp(self.τ * self.b_11) + exp(2 * self.τ * self.b_11) + 2 * self.τ * self.b_11) * self.δ_1 - 4 * self.b_11 * (1 - exp(self.τ * self.b_11) + self.τ * self.b_11) * self.λ_1)) / (4 * self.b_11**3)
       self._A = expression
-    
+
     return self._A
 
   @property
   def B(self):
     if self._B is None:
-      expression = ((exp(self.τ * self.b11)) * self.δ0) / self.b11
+      expression = ((exp(self.τ * self.b_11)) * self.δ_1) / self.b_11
       self._B = expression
 
-    return self._B  
+    return self._B
 
   def A(self, τ, θ):
-    return (θ[1] * ((3 - 4 * exp(τ * θ[0]) + exp(2 * τ * θ[0]) + 2 * τ * θ[0]) * θ[1] - 4 * θ[0] * (1 - exp(τ * θ[0]) + τ * θ[0]) * θ[2])) / (4. * θ[0]**3)
+    return (θ[1] * ((3 - 4 * exp(τ * θ[0]) + exp(2 * τ * θ[0]) + 2 * τ * θ[0]) * θ[1] - 4 * θ[0] * (1 - exp(τ * θ[0]) + τ * θ[0]) * θ[2])) / (4 * θ[0]**3)
 
   def B(self, τ, θ):
     return ((exp(τ * θ[0])) * θ[1]) / θ[0]
 
   def Γ0(self, θ):
-    # f = lambdify(self.τ, -self.A.subs([(self.b11, θ[0]), (self.λ1, θ[2]), (self.δ0, θ[1])]))
+    # f = lambdify(self.τ, -self.A.subs([(self.b_11, θ[0]), (self.λ_1, θ[2]), (self.δ_1, θ[1])]))
     # return f(self.without_error_maturity)
     return -self.A(self.without_error_maturity, θ)
 
   def Γ0_ext(self, θ):
-    # f = lambdify(self.τ, -self.A.subs([(self.b11, θ[0]), (self.λ1, θ[2]), (self.δ0, θ[1])]))
+    # f = lambdify(self.τ, -self.A.subs([(self.b_11, θ[0]), (self.λ_1, θ[2]), (self.δ_1, θ[1])]))
     # return f(self.with_error_maturity)
     return -self.A(self.with_error_maturity, θ)
 
@@ -57,4 +57,33 @@ class A01:
   def Γ_ext(self, θ):
     return self.B(self.with_error_maturity, θ)
 
+  def l_x(self, k, Δ, x, x_0, θ):
+    sum = 1
 
+    for i in range(1, k + 1):
+      sum = sum + self.c(i, x, x_0, θ) * Δ**i / factorial(i)
+
+    return self.l_x_0(Δ, x, x_0, θ) + log(sum)
+
+  def l_x_0(self, Δ, x, x_0, θ):
+    return -log(sqrt(2 * pi * Δ)) - (x - x_0)**2 / (2 * Δ) + θ[0] * x**2 / 2 - θ[0] * x_0**2 / 2
+
+  def c(self, k, x, x_0, θ):
+    def coefficient_not_found(x, x_0, θ):
+      raise BaseException("No coefficient " + str(k) + " found!")
+
+    function_name = 'c' + str(k)
+    function = getattr(self, function_name, coefficient_not_found)
+
+    return function(x, x_0, θ)
+
+  def c1(self, x, x_0, θ):
+    return θ[0] / 6 * (3 + θ[0] * x**2 + θ[0] * x * x_0 + θ[0] * x_0**2)
+
+  def c2(self, x, x_0, θ):
+    return θ[0]**2 / 36 * (3 + 6 * θ[0] * x**2 + θ[0]**2 * x**4 + 2 * θ[0] * x * x_0 * (3 + θ[0] * x**2) + 3 * θ[0] * x_0**2 * (2 + θ[0] * x**2) - 2 * θ[0]**2 * x * x_0**3 + θ[0]**2 * x_0**4)
+
+  def l_g(self, k, Δ, g, g_0, θ):
+    x = (g - self.Γ0(θ)) / self.Γ(θ)
+    x_0 = (g_0 - self.Γ0(θ)) / self.Γ(θ)
+    return log(abs(1 / self.Γ(θ))) + self.l_x(k, Δ, x, x_0, θ)
--- univariate_estimates.py
+++ univariate_estimates.py
@@ -18,6 +18,7 @@ FILENAME = "data/yields.xls"
 book = xlrd.open_workbook(FILENAME)
 sheet_index = book.sheet_names().index('ZEROYLD')
 sheet = book.sheet_by_index(sheet_index)
+delta = 1/12
 
 def prepare_data():
   first_row = sheet.row_values(0)
@@ -42,7 +43,8 @@ def prepare_data():
 
 def likelihood(θ, model, observations):
   yield_errors = []
-  for i in range(0, observations.length()):
+  n = observations.length()
+  for i in range(0, n):
     gt = observations[i, 1, model.n]
     gt_ext = observations[i, model.n + 1, 2 * model.n]
     xt = (gt - model.Γ0(θ)) / model.Γ(θ)
@@ -52,10 +54,22 @@ def likelihood(θ, model, observations):
   σ = np.std(yield_errors)
   error_distribution = norm(0, σ)
 
-  joint_errors_likelihood = inject(lambda memo, x: memo + math.log(x), 1.0, list(map(lambda x: error_distribution.pdf(x), yield_errors)))
+  # joint_errors_likelihood = inject(lambda memo, x: memo + math.log(x), 0.0, list(map(lambda x: error_distribution.pdf(x), yield_errors)))
+  observations_likelihood = 0.0
+  for i in range(1, n):
+    g = observations[i, 1, model.n]
+    g_0 = observations[i - 1, 1, model.n]
+    observations_likelihood += model.l_g(2, delta, g, g_0, θ)
 
-  return -joint_errors_likelihood
+  # return -(joint_errors_likelihood + observations_likelihood) / n
+  # return -joint_errors_likelihood / n
+  return -observations_likelihood / n
 
-theta0 = [0.1, 0.1, 0.1]
-a01_estimates = minimize(likelihood, theta0, args=(A01(), YieldSeries(table = prepare_data(), nfactors = 1),), method='nelder-mead', options={'xtol': 1e-8, 'disp': True})
+theta0 = [-0.03, -0.01, 1]
+model = A01()
+y_s = YieldSeries(table = prepare_data(), nfactors = 1)
+a01_estimates = minimize(likelihood, theta0, args=(model, y_s,), method='nelder-mead', options= { 'xtol': 1e-6, 'disp': True, 'maxiter': 1000 })
+# for i in range(0, y_s.length()):
+#   gt = y_s[i, 1, model.n]
+#   print((gt - model.Γ0(a01_estimates.x)) / model.Γ(a01_estimates.x))
 print(a01_estimates.x)
