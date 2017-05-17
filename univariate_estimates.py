c5a8f8b741ce55b3c3c5fa2ece8939f4fc56ad32
maxmilan <max231194@gmail.com>
2017-05-17 19:11:02 +0300

    Check results

--- univariate_estimates.py
+++ univariate_estimates.py
@@ -45,20 +45,18 @@ def prepare_data():
 
 def likelihood(θ, model, observations, likelihood_method):
   yield_errors = []
-  # print(θ)
   n = observations.length()
-  for i in range(0, n):
-    gt = observations[i, 1, model.n]
-    gt_ext = observations[i, model.n + 1, 2 * model.n]
-    xt = (gt + model.Γ_0(θ)) / model.Γ(θ)
-    gt_calculated = -model.Γ_0_ext(θ) + model.Γ_ext(θ) * xt
-    yield_errors.append((gt_calculated - gt_ext)**2)
+  # for i in range(0, n):
+  #   gt = observations[i, 1, model.n]
+  #   gt_ext = observations[i, model.n + 1, 2 * model.n]
+  #   xt = (gt + model.Γ_0(θ)) / model.Γ(θ)
+  #   gt_calculated = -model.Γ_0_ext(θ) + model.Γ_ext(θ) * xt
+  #   yield_errors.append(gt_calculated - gt_ext)
 
   # σ = np.std(yield_errors)
   # error_distribution = norm(0, σ)
 
-  # joint_errors_likelihood = inject(lambda memo, x: memo + math.log(x), 0.0, list(map(lambda x: error_distribution.pdf(x), yield_errors)))
-
+  # joint_errors_likelihood = inject(lambda memo, x: memo + log(x), 0.0, list(map(lambda x: error_distribution.pdf(x), yield_errors)))
 
   observations_likelihood = 0.0
   for i in range(1, n):
@@ -67,16 +65,25 @@ def likelihood(θ, model, observations, likelihood_method):
     observations_likelihood += model.l_g(likelihood_method, delta, g, g_0, θ)
 
   # return -(joint_errors_likelihood + observations_likelihood) / n
-  # return -joint_errors_likelihood / n
-  return -(observations_likelihood) / n
+  return -observations_likelihood / n
+
+# theta0 = [-0.185793266805, 0.0264870240577, -0.866720304618]
+# theta1 = [-0.0999041150336, -0.106014406172, 0.242671357612]
+# -model01.γ_0(2, theta0) + model01.γ(2, theta0) * xt0
+# yields = y_s.table[0]
+# gt = yields[1]
+# xt0 = (gt + model01.γ_0(1/12, theta0)) / model01.γ(1/12, theta0)
+# xt1 = (gt + model01.γ_0(1/12, theta1)) / model01.γ(1/12, theta1)
+
+# code.interact(local=dict(globals(), **locals()))
 
 y_s = YieldSeries(table = prepare_data(), nfactors = 1)
-# print("Number of observations: " + str(y_s.length()) + "\n")
+print("Number of observations: " + str(y_s.length()) + "\n")
 
-# print("-------------------------- A01 Model --------------------------------")
+print("-------------------------- A01 Model --------------------------------")
 theta0 = [-0.03, 0.05, 1]
-# theta0 = [-0.541288257921, 0.0270055072839, -1.39696599475]
 model01 = A01()
+
 print("__________________________ TRUE likelihood __________________________")
 a01_true = minimize(likelihood, theta0, args=(model01, y_s, "true",), method='nelder-mead', options= { 'xtol': 1e-6, 'disp': True, 'maxiter': 1000 })
 model01.print_params(a01_true.x)
